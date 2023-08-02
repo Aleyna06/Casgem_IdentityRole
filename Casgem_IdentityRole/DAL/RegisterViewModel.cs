82ac8dadd3c95fc342f6d851c586c60b17c01e5a
aleyna çelik <celikaleyna71@gmail.com>
2023-08-02 14:37:38 +0300

    Add project files.

---
+++ Casgem_IdentityRole/DAL/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Casgem_IdentityRole.DAL
+{
+    public class RegisterViewModel
+    {
+        public string NameSurname { get; set; }
+        public string Username { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+       
+    }
+}
